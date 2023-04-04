cf45a54a24a62b8646da266ef47d26799208e8ef
Olababs98 <babatundesaheed389@gmail.com>
2023-04-04 15:43:46 -0700

    python

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -3,12 +3,21 @@
 
 
 class Rectangle:
+    """Represent a rectangle"""
+
     def __init__(self, width=0, height=0):
+        """Initialize a new Rectangle.
+
+        Args:
+            width (int): The width of the new rectangle.
+            height (int): The height of the new rectangle.
+        """
         self.width = width
         self.height = height
 
     @property
     def width(self):
+        """Get the width of the Rectangle."""
         return self.__width
 
     @width.setter
@@ -21,6 +30,7 @@ class Rectangle:
             self.__width = value
 
     @property
+    """get the height of Rectangle."""
     def height(self):
         return self.__height
 
@@ -34,12 +44,15 @@ class Rectangle:
             self.__height = value
 
     def area(self):
+        """Return the area of the Rectangle"""
         return self.__width * self.__height
 
     def perimeter(self):
+        """Return the perimeter of the Rectangle"""
         return 2 * (self.__width + self.__height)
 
     def __str__(self):
+        """Return the printable representation of the rectangle."""
         if self.__width == 0 or self.__height == 0:
             return ""
         else:
