bc8e327760804c0c018e1811ba1f410de452b8f5
Olababs98 <babatundesaheed389@gmail.com>
2023-04-04 16:56:48 -0700

    python

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -1,22 +1,27 @@
 #!/usr/bin/python3
-"""
-This module defines a Rectangle class.
+"""This module defines a Rectangle class.
 
 The Rectangle class represents a rectangle, with width and height attributes
-and methods to calculate its area and perimeter. 
-
-The class also defines a
+and methods to calculate its area and perimeter. The class also defines a
 string representation and a way to recreate instances using eval().
 """
 
-
 class Rectangle:
+    """Represents a rectangle."""
+
     def __init__(self, width=0, height=0):
+        """Initializes a new Rectangle.
+
+        Args:
+            width (int): The width of the new rectangle.
+            height (int): The height of the new rectangle.
+        """
         self.width = width
         self.height = height
 
     @property
     def width(self):
+        """Gets or sets the width of the rectangle."""
         return self.__width
 
     @width.setter
@@ -29,6 +34,7 @@ class Rectangle:
 
     @property
     def height(self):
+        """Gets or sets the height of the rectangle."""
         return self.__height
 
     @height.setter
@@ -40,17 +46,21 @@ class Rectangle:
         self.__height = value
 
     def area(self):
+        """Returns the area of the rectangle."""
         return self.width * self.height
 
     def perimeter(self):
+        """Returns the perimeter of the rectangle."""
         if self.width == 0 or self.height == 0:
             return 0
         return 2 * (self.width + self.height)
 
     def __str__(self):
+        """Returns the string representation of the rectangle."""
         if self.width == 0 or self.height == 0:
             return ""
         return "\n".join(["#" * self.width] * self.height)
 
     def __repr__(self):
+        """Returns a string representation of the rectangle."""
         return f"Rectangle({self.width}, {self.height})"
