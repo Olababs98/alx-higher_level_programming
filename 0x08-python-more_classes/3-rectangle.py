8e20ef54788d1f19ba2f1da80d7c82b46ea9787f
Olababs98 <babatundesaheed389@gmail.com>
2023-04-04 16:33:18 -0700

    python

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-"""This module defines a Rectangle class.
+"""
+This module defines a Rectangle class.
 
 The Rectangle class represents a rectangle, with width and height attributes methods to calculate its area and perimeter.
 """
+
 
 class Rectangle:
     """Represent a rectangle"""
