7f0c883172a9bfe93bafb78c2e45d0c542dd33f0
Olababs98 <babatundesaheed389@gmail.com>
2023-04-04 18:56:28 -0700

    python

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -24,7 +24,7 @@ class Rectangle:
         elif value < 0:
             raise ValueError('width must be >= 0')
         else:
-        self.__width = value
+            self.__width = value
 
     @property
     def height(self):
@@ -37,7 +37,7 @@ class Rectangle:
         elif value < 0:
             raise ValueError('height must be >= 0')
         else:
-        self.__height = value
+            self.__height = value
 
     def area(self):
         return self.__width * self.__height
@@ -52,4 +52,4 @@ class Rectangle:
             rect = []
             for i in range(self.__height):
                 rect.append("#" * self.__width)
-        return "\n".join(rect)
+            return "\n".join(rect)
