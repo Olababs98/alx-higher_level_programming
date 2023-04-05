422ae389adc48b1b6dea4a7b9834dfea417bb31b
Olababs98 <babatundesaheed389@gmail.com>
2023-04-05 13:10:52 -0700

    python

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -10,16 +10,18 @@ class Rectangle:
 
 
     def __init__(self, width=0, height=0):
-
+        """Initialize a new rectangle."""
         self.width = width
         self.height = height
 
     @property
     def width(self):
+        """Get the width of the rectangle."""
         return self.__width
 
     @width.setter
     def width(self, value):
+        """Set the width of the rectangle."""
         if not isinstance(value, int):
             raise TypeError('width must be an integer')
         elif value < 0:
@@ -29,10 +31,12 @@ class Rectangle:
 
     @property
     def height(self):
+        """Get the height of the rectangle."""
         return self.__height
 
     @height.setter
     def height(self, value):
+        """Set the height of the rectangle."""
         if not isinstance(value, int):
             raise TypeError('height must be an integer')
         elif value < 0:
@@ -41,14 +45,17 @@ class Rectangle:
             self.__height = value
 
     def area(self):
+        """Calculate the area of the rectangle."""
         return self.__width * self.__height
 
     def perimeter(self):
+        """Calculate the perimeter of the rectangle."""
         if self.__width == 0 or self.__height == 0:
             return (0)
         return 2 * (self.__width + self.__height)
 
     def __str__(self):
+        """Return the printable of the Rectangle."""
         if self.__width == 0 or self.__height == 0:
             return ""
         else:
