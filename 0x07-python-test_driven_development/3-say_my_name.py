1e563cd1d707abd3499cd041879388891cc23780
Olababs98 <babatundesaheed389@gmail.com>
2023-04-06 14:47:48 -0700

    python3

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -15,14 +15,11 @@ def say_my_name(first_name, last_name=""):
     # Validate the type of input arguments
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
-    if not isinstance(last_name, str):
+    if last_name != "" and not isinstance(last_name, str):
         raise TypeError("last_name must be a string")
 
     # Print the name
     if last_name:
-        if isinstance(first_name, str):
-            print(f"My name is {first_name} {last_name}")
-        else:
-            raise TypeError("first_name must be a string")
+        print(f"My name is {first_name} {last_name}")
     else:
         print(f"My name is {first_name}")
