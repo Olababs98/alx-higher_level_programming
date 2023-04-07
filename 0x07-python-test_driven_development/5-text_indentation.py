ef0bb99d44262b189463bd44a9be2f0fd50317e3
Olababs98 <babatundesaheed389@gmail.com>
2023-04-07 13:23:50 -0700

    python3

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -3,7 +3,7 @@
 
 
 def text_indentation(text):
-    """ prints the given text with two new lines after each occurence."""
+    """ prints the given text with two new lines after each occurence '.','?'and ':'."""
 
     # Validate the type of input argument
     if not isinstance(text, str):
@@ -14,12 +14,12 @@ def text_indentation(text):
 
     # Loop through each character of the input string
     for char in text:
-        # Add the character to the output string
-        output += char
+    # Add the character to the output string
+    output += char
 
-        # If the character is '.', '?' or ':', add two new lines to the output string
-        if char in ('.', '?', ':'):
-            output += "\n\n"
+    # If the character is '.', '?' or ':', add two new lines to the output string
+    if char in ('.', '?', ':'):
+        output += "\n\n"
 
     # Print the output string
     print(output.strip())
