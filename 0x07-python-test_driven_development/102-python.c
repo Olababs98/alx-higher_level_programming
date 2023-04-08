20071bd9e6849ceedd2158a8ca405787bd59deca
Olababs98 <babatundesaheed389@gmail.com>
2023-04-08 13:10:34 -0700

    python3

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -1,4 +1,4 @@
-#include "python.h"
+#include <python.h>
 
 /**
  * print_python_string - Prints information about Python strings.
@@ -6,23 +6,19 @@
  */
 void print_python_string(PyObject *p)
 {
-	long int length;
+    long int length;
 
-	fflush(stdout);
+    printf("[.] string object info\n");
 
-	printf("[.] string object info\n");
-	if (strcmp(p->ob_type->tp_name, "str") != 0)
-	{
-		printf("  [ERROR] Invalid String Object\n");
-		return;
-	}
+    if (p == NULL || !PyUnicode_Check(p))
+    {
+        printf("  [ERROR] Invalid String Object\n");
+        return;
+    }
 
-	length = ((PyASCIIObject *)(p))->length;
+    length = PyUnicode_GetLength(p);
 
-	if (PyUnicode_IS_COMPACT_ASCII(p))
-		printf("  type: compact ascii\n");
-	else
-		printf("  type: compact unicode object\n");
-	printf("  length: %ld\n", length);
-	printf("  value: %ls\n", PyUnicode_AsWideCharString(p, &length));
+    printf("  type: %s\n", p->ob_type->tp_name);
+    printf("  length: %ld\n", length);
+    printf("  value: %ls\n", PyUnicode_AsWideCharString(p, &length));
 }
---
+++ 0x07-python-test_driven_development/python.h
@@ -0,0 +1,7 @@
+#ifndef PYTHON_H
+#define PYTHON_H
+
+typedef struct _object PyObject;
+typedef struct PyVarObject PyUnicodeObject;
+
+#endif
