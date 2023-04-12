c06b62fd71c570311ed9ce98e4a3317e41609296
Olababs98 <babatundesaheed389@gmail.com>
2023-04-12 14:11:33 -0700

    inherit

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -3,7 +3,7 @@
 
 
 def is_same_class(obj, a_class):
-     """Check if an objects is exactly an instance of a given class."""
+    """check if an object is exactly an instance of a given class."""
 
     if type(obj) == a_class:
         return True
