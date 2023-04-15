61ccd6c031cd3b4281df14eb2468322c603ac293
Olababs98 <babatundesaheed389@gmail.com>
2023-04-15 06:16:25 -0700

    inp/out

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Defines a file-writing fuction."""
+
+
+def write_file(filename="", text=""):
+    """Write a string to a UTF8 text file."""
+    with open(filename, "W", encoding=utf-8") as f:
+        return f.write(text)
