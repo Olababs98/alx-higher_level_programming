b8dbb7d7984c8dd78b80f9e72b1806f7345b022c
Olababs98 <babatundesaheed389@gmail.com>
2023-04-15 06:29:56 -0700

    inp/out

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -3,6 +3,13 @@
 
 
 def write_file(filename="", text=""):
-    """Write a string to a UTF8 text file."""
+    """Write a string to a UTF8 text file.
+
+    Args:
+        filename (str): The name of the file to write.
+        text (str): The text to write to the file.
+    Returns:
+        The number of characters written.
+    """
     with open(filename, "W", encoding="utf-8") as f:
         return f.write(text)
