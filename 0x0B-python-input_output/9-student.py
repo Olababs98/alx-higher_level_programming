e30b2f87084047440be3fb6f9c865e8a7d6a12c3
Olababs98 <babatundesaheed389@gmail.com>
2023-04-16 03:22:52 -0700

    inp/out

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -6,11 +6,17 @@ class student:
     """Represent a student."""
 
     def __init__(self, first_name, last_name, age):
-        """Initialize a new student."""
+        """Initialize a new student.
+
+        Args:
+            first_name (str): The first name of the student
+            last_name (str): The last name of the student
+            age (int): The age of the student
+            """
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
-        """Get a dic rep of the student."""
+        """Get a dictionary representation of the student."""
         return self.__dict__
