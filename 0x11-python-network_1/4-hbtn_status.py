4781ac51e335c2b40a40bc9834427d96d2728ae4
Olababs98 <babatundesaheed389@gmail.com>
2023-07-05 15:00:02 -0700

    net1

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-"""fetches https://intranet.hbtn.io/status."""
+"""fetches https://0.0.0.0:5050/status."""
 import requests
 
 
 if __name__ == "__main__":
+    url = "http://0.0.0.0:5050/status"
     r = requests.get("https://intranet.hbtn.io/status")
     print("Body response:")
     print("\t- type: {}".format(type(r.text)))
